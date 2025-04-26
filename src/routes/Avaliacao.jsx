a9361876ebacddaed94f57d0aace59507f894461
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2025-04-26 15:59:05 -0300

    Adiciona componentes e rotas

---
+++ src/routes/Avaliacao.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Avaliacao = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Avaliacao
---
+++ src/routes/Sobre.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sobre = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Sobre
