f74df55b9e2b7eff8bbdee96b7a8f5753170f46d
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2025-04-26 16:18:53 -0300

    Adiciona Error.jsx e estrutura do App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Error from './routes/Error'
 
+import Home from './routes/Home'
+import Sobre from './routes/Sobre'
+import Funcionalidades from './routes/Funcionalidades'
+import Avaliacao from './routes/Avaliacao'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="*" element={<Error />} />
+          <Route path="/" element={<Home />} />
+          <Route path="Sobre" element={<Sobre />} />
+          <Route path="Funcionalidades" element={<Funcionalidades />} />
+          <Route path="Avaliacao" element={<Avaliacao />} />
+        </Routes>
+      <Footer/>  
+    </BrowserRouter>
   )
 }
 
